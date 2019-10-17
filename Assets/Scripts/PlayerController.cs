2c4ec0d83e9098ff593d267b9beeb506879a9005
unknown <tolia345@mail.ru>
2019-10-18 00:49:01 +0300

    load

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody rb;
+    [SerializeField] float speed;
+    [SerializeField] Text scoreText;
+    [SerializeField] Text winText;
+    int score;
+
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        score = 0;
+        SetCoutText();
+        winText.text = "";
+    }
+
+	void FixedUpdate()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(hor, 0.0f, ver);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider pickUp)
+    {
+        if (pickUp.gameObject.CompareTag("Pick Up"))
+        {
+            pickUp.gameObject.SetActive(false);
+            score++;
+            SetCoutText();
+        }
+        //Destroy(pickUp.gameObject);
+    }
+
+    void SetCoutText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+        if (score >= 12)
+            winText.text = "So EZ!";
+    }
+
+
+}
